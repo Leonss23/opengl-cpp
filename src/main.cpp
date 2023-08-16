4fadd280deba6edb51c952057a0b4890b7484dc9
Leonardo Gatti [Leonss23] <leonardogattidev@gmail.com>
2023-08-16 03:21:36 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include "config.h"
+
+int main() {
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+        }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "basic_window", NULL, NULL);
+
+    if (window == NULL) {
+        std::cout << "GLFW failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+        }
+
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+
+    glViewport(0, 0, 1920, 1080);
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+
+        }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+    };
