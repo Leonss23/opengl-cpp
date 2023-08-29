5a164a2d8e6f9be889a59a7fcf401cd6830071ef
Leonss23 <leonardogattidev@gmail.com>
2023-08-29 01:45:02 -0300

    added utils mod with `get_file_contents` function

---
+++ src/include/utils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+std::string get_file_contents(const char* filename);
---
+++ src/utils.cpp
@@ -0,0 +1,17 @@
+#include "utils.h"
+
+std::string get_file_contents(const char* filename) {
+    std::string filepath = "shaders/" + std::string(filename);
+    std::ifstream in(filepath, std::ios::binary);
+    if (in) {
+        std::string contents;
+        in.seekg(0, std::ios::end);
+        contents.resize(in.tellg());
+        in.seekg(0, std::ios::beg);
+        in.read(&contents[0], contents.size());
+        in.close();
+        return contents;
+        }
+    std::cerr << "couldn't find " << filename << std::endl;
+    throw(errno);
+    }
