bbf52c6069c4995ee65441499f26bbcaad6cdfc9
Leonss23 <leonardogattidev@gmail.com>
2023-08-28 23:22:17 -0300

    initialized window branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include "deps.h"
-#include "ShaderProgram.h"
-#include "VAO.h"
-#include "VBO.h"
-#include "EBO.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-unsigned int newShader(const char* shaderSource, GLenum shaderType);
-unsigned int newShaderProgram(GLuint* shaders, uint16_t shaderCount);
 
-const unsigned int SCREEN_WIDTH = 1920;
-const unsigned int SCREEN_HEIGHT = 1080;
+const int SCR_WIDTH = 1000;
+const int SCR_HEIGHT = 800;
 
 int main() {
     // GLFW Init
@@ -28,7 +22,7 @@ int main() {
 #endif
 
     // GLFW Window Init
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Learning OpenGL :D", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learning OpenGL :D", NULL, NULL);
 
     if (window == NULL) {
         std::cout << "GLFW failed to create window" << std::endl;
@@ -46,39 +40,7 @@ int main() {
     gladLoadGL();
 
     // OpenGL Setup
-    glViewport(0, 0, 1920, 1080);
-
-    ShaderProgram shaderProgram = ShaderProgram("default.vert", "default.frag");
-
-    // Set up GL buffers
-    // square vertices
-    float vertices[] = {
-    0.5f,  0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f
-        };
-    // square indices
-    unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3
-        };
-
-    VAO VAO1;
-    VAO1.Bind();
-
-    VBO VBO1(vertices, sizeof(vertices));
-    EBO EBO1(indices, sizeof(indices));
-
-    VAO1.LinkVBO(VBO1, 0);
-
-    VBO1.Unbind();
-    VAO1.Unbind();
-    EBO1.Unbind();
-
-
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
     while (!glfwWindowShouldClose(window)) {
         // logic
@@ -88,20 +50,10 @@ int main() {
         glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        shaderProgram.Use();
-        VAO1.Bind();
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
-
         // submit
         glfwSwapBuffers(window);
         glfwPollEvents();
         }
-
-    // Clean Up OpenGL
-    VAO1.Delete();
-    VBO1.Delete();
-    EBO1.Delete();
-    shaderProgram.Delete();
 
     glfwDestroyWindow(window);
     glfwTerminate();
