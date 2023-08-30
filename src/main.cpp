73d84cd368c4576c00c1a577ab8fc32183e0fd0d
Leonss23 <leonardogattidev@gmail.com>
2023-08-29 21:29:09 -0300

    changed vertex data into a square

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,18 @@ int main() {
 
     // Vertex Data
     GLfloat vertices[] = {
-            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // left
-            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // right
-            0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // top
+            -0.5f,  -0.5f,  0.0f,   // bottom-left  0
+            1.0f,   0.0f,   0.0f,   // red
+            -0.5f,  0.5f,   0.0f,   // top-left     1
+            0.0f,   1.0f,   0.0f,   // green
+            0.5f,   0.5f,   0.0f,   // top-right    2
+            0.0f,   0.0f,   1.0f,   // blue
+            0.5f,   -0.5f,  0.0f,   // bottom-right 3
+            1.0f,   1.0f,   1.0f,   // white
         };
     GLuint indices[] = {
-        0, 1, 2
+        0, 1, 2,
+        2, 3, 0
         };
     GLuint indicesCount = sizeof(indices) / sizeof(indices[0]);
 
