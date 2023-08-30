9e9c1125607a94c330479ac63ca647e682034ffa
Leonss23 <leonardogattidev@gmail.com>
2023-08-30 20:20:34 -0300

    added `glm` lib

--- src/include/config.h
+++ src/include/config.h
@@ -4,6 +4,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "window.h"
 #include "utils.h"
