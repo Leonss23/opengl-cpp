2a4fbd0af21fb6e54f03287176bd11f3156e5674
Leonss23 <leonardogattidev@gmail.com>
2023-08-30 00:56:40 -0300

    updated complete code for `textures`

--- res/shaders/default.vert
+++ res/shaders/default.vert
@@ -2,12 +2,15 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTex;
 
 out vec3 color;
+out vec2 texCoord;
 
 uniform float scale;
 
 void main() {
     gl_Position = vec4(aPos.x * scale, aPos.y * scale, aPos.z * scale, 1.0);
     color = aColor;
+    texCoord = aTex;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,21 @@ int main() {
 
     // Vertex Data
     GLfloat vertices[] = {
-            -0.5f,  -0.5f,  0.0f,   // bottom-left  0
-            1.0f,   0.0f,   0.0f,   // red
-            -0.5f,  0.5f,   0.0f,   // top-left     1
-            0.0f,   1.0f,   0.0f,   // green
-            0.5f,   0.5f,   0.0f,   // top-right    2
-            0.0f,   0.0f,   1.0f,   // blue
-            0.5f,   -0.5f,  0.0f,   // bottom-right 3
-            1.0f,   1.0f,   1.0f,   // white
+        //  Position            /   Color               /   Texture         //  
+        //  x       y       z   /   r       g       b   /   x       y       //  
+            -0.5f,  -0.5f,  0.0f,   1.0f,   0.0f,   0.0f,   0.0f,   0.0f,   //  bottom-left     red
+            -0.5f,  0.5f,   0.0f,   0.0f,   1.0f,   0.0f,   0.0f,   1.0f,   //  top-left        green
+            0.5f,   0.5f,   0.0f,   0.0f,   0.0f,   1.0f,   1.0f,   1.0f,   //  top-right       blue
+            0.5f,   -0.5f,  0.0f,   1.0f,   1.0f,   1.0f,   1.0f,   0.0f,   //  bottom-right    white
         };
     GLuint indices[] = {
         0, 1, 2,
         2, 3, 0
         };
     GLuint indicesCount = sizeof(indices) / sizeof(indices[0]);
+    GLenum attrType = GL_FLOAT;
+    GLuint vertStride = 8;
+    GLsizeiptr vertSize = vertStride * sizeof(vertices[0]);
 
     ShaderProgram ShaderProgram("default.vert", "default.frag");
 
@@ -28,17 +29,45 @@ int main() {
     BufferObject VBO1(GL_ARRAY_BUFFER, vertices, sizeof(vertices));
     BufferObject EBO1(GL_ELEMENT_ARRAY_BUFFER, indices, sizeof(indices));
 
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(GLfloat), 0);
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    VAO1.LinkAttrib(VBO1, 0, 3, attrType, vertSize, 0);
+    VAO1.LinkAttrib(VBO1, 1, 3, attrType, vertSize, (void*)(3 * sizeof(GLfloat)));
+    VAO1.LinkAttrib(VBO1, 2, 2, attrType, vertSize, (void*)(6 * sizeof(GLfloat)));
 
     VBO1.Unbind();
     VAO1.Unbind();
     EBO1.Unbind();
 
+    // Uniforms
     GLuint scaleUniform = glGetUniformLocation(ShaderProgram.ID, "scale");
 
     // Drawing settings
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    // Texture
+    int texWidth, texHeight, texColorChannelCount;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* texData = stbi_load("res/textures/don_trolleone.png", &texWidth, &texHeight, &texColorChannelCount, 0);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(texData);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    GLuint tex0Uni = glGetUniformLocation(ShaderProgram.ID, "tex0");
+    ShaderProgram.Use();
+    glUniform1i(tex0Uni, 0);
 
     while (!glfwWindowShouldClose(window)) {
         // logic
@@ -51,6 +80,7 @@ int main() {
         ShaderProgram.Use();
         glUniform1f(scaleUniform, 1.0f);
         VAO1.Bind();
+        glBindTexture(GL_TEXTURE_2D, texture);
         glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, 0);
 
         // submit
@@ -61,6 +91,7 @@ int main() {
     VAO1.Delete();
     VBO1.Delete();
     EBO1.Delete();
+    glDeleteTextures(1, &texture);
     ShaderProgram.Delete();
 
     cleanupGlfw(window);
